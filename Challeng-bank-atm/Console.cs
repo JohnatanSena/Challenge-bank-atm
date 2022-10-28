928f595f3edb0c8a5b8bd3773515553d4ae7791f
Johnatan Sena DEV <johnatansena95@gmail.com>
2022-10-28 06:25:37 -0300

    Create Project

---
+++ Challeng-bank-atm/Console.cs
@@ -0,0 +1,79 @@
+﻿using Challenge_bank_atm;
+
+Console.WriteLine("Qual valor do saque");
+var atm = new AtmSystem();
+var withdrawValue = Convert.ToInt32(Console.ReadLine());
+atm.CalculateBankNotes(withdrawValue);
+
+namespace Challenge_bank_atm
+{
+
+    public class AtmSystem
+    {
+        public void CalculateBankNotes(long withdrawValue)
+        {
+            int ced = 100;
+            int totalced = 0;
+            var result = "";
+
+            while (true)
+            {
+                if (withdrawValue >= ced)
+                {
+                    withdrawValue -= ced;
+                    totalced += 1;
+                }
+
+                else
+                {
+                    if (totalced > 0)
+                    {
+                        result += $"{totalced} de R$ {ced},00 \n";
+                    }
+
+                    if (ced == 100)
+                    {
+                        ced = 50;
+                    }
+                    else if (ced == 50)
+                    {
+                        ced = 20;
+                    }
+                    else if (ced == 20)
+                    {
+                        ced = 10;
+
+                    }
+                    else if (ced == 10)
+                    {
+                        ced = 5;
+                    }
+
+                    else
+                    {
+                        ced = 2;
+                    }
+                    totalced = 0;
+
+                    if (withdrawValue == 0 || withdrawValue == 1)
+                    {
+                        break;
+                    }
+                }
+
+            }
+            if (withdrawValue == 1)
+            {
+                Console.WriteLine("Valor do saque invalido");
+
+            }
+            else if (withdrawValue == 0)
+            {
+                Console.WriteLine(result);
+            }
+
+        }
+    }
+}
+
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Projeto - Caixa eletrônico.
+
+## A estrutura do projeto:
+* O caixa eletrônico terá um total de notas disponíveis para saques. Terá cédulas disponíveis: R$2, R$5, R$10, R$20, R$50 e R$100.
+* Após interação do usuário com o caixa eletrônico, será disponibilizado as cédulas citadas acima. 
+* A saída de cédulas terá uma lógica, onde não será possivel ter saques com valores que necessitam de cédulas de R$ 1. 
+
